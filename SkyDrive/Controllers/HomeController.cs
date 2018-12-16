efbf921f49a48943f04b0f18703e3f0dac434e75
matthewking4 <matthewking4@sky.uk>
2018-12-16 18:59:47 +0000

    Sign in template

---
+++ SkyDrive/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SkyDrive.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace SkyDrive.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Description page.";
+
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Files()
+        {
+            ViewData["Message"] = "Your Files page.";
+
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Notes()
+        {
+            ViewData["Message"] = "Your Note page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
